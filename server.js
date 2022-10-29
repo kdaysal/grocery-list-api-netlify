0c5434f4e171e0a6f6333b4883e888253f957a2f
Kevin Daysal <kdaysal123@gmail.com>
2022-10-28 22:00:07 -0400

    First attempt to replace localhost path with mongodb-Atlas connection uri
    -In server.js the variable is called "DATABASE_URL" instead of connection uri.
    
    -Also added DATABASE_URL to my environment variable config settings in Heroku.

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }); //see '.env' file for corresponding DATABASE_URL
+//note - in .env file, the original DATABASE_URL line for local testing was: DATABASE_URL=mongodb://localhost/grocery-list
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/grocery-list', { useNewUrlParser: true }); //see '.env' file for corresponding DATABASE_URL
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error)); //if there is an error connecting to my db, log the error
 db.once('open', () => console.log('connected to database, yay!')); //if I successfully connect, log this message (will only run once)
