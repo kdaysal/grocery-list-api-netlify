e0014841662150d111c87a8223cc944087874ceb
Kevin Daysal <kdaysal123@gmail.com>
2023-02-11 14:58:29 -0500

    Attempting to re-order how I declared CORS in my server.js
    -This is an attempt to fix the CORS error I'm getting only on my 'POST' call when trying to create a new user via the client-side app.

--- server.js
+++ server.js
@@ -3,7 +3,11 @@
 require('dotenv').config(); //load all environment variables from the .env file
 
 const express = require('express');
+const cors = require('cors'); // implement CORS (Cross-Origin-Resource-Sharing) so the receiving server can identify where requests are coming from and allow or disallow them
 const app = express();
+app.use(cors()); //leaving the default set up to allow requests from ALL origins (during development)
+app.use(express.json()) //this allows the server to accept JSON as a BODY (instead of a POST element or a GET element, etc)
+
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 
@@ -12,11 +16,6 @@ mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/grocery-list',
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error)); //if there is an error connecting to my db, log the error
 db.once('open', () => console.log('connected to database, yay!')); //if I successfully connect, log this message (will only run once)
-
-app.use(express.json()) //this allows the server to accept JSON as a BODY (instead of a POST element or a GET element, etc)
-
-const cors = require('cors'); // implement CORS (Cross-Origin-Resource-Sharing) so the receiving server can identify where requests are coming from and allow or disallow them
-app.use(cors()); //leaving the default set up to allow requests from ALL origins (during development)
 
 //Setting up the routes
 const usersRouter = require('./routes/users'); //since all routes will stem from the root url + '/users', this lets me shorthand it so in my routes (in users.js) I don't have to specify '/users' for each one.
